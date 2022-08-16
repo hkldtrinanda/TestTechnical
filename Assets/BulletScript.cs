cb02db7a4f00a136881a215523278bf1ae346c70
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-08-16 12:57:27 +0700

    commit

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	GunScript gun;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gun = GameObject.FindGameObjectWithTag("Gun").GetComponent<GunScript>();	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "box")
+		{
+			gun.TargetHit(col.gameObject);
+		}
+
+		Destroy(gameObject);
+	}
+}
